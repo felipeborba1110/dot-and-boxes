5eb13a498656e95498a017da9598ce0fdf15a1ba
Felipe Borba <162516359+felipeborba1110@users.noreply.github.com>
2024-11-25 15:02:37 -0300

    Update tabuleiro.py

--- server/tabuleiro.py
+++ server/tabuleiro.py
@@ -1,5 +1,6 @@
 from cliente.terminal import T_BBLUE, T_FBLACK, T_RESET, T_BGREEN
 
+
 class Tabuleiro:
 
     _x: int
@@ -13,6 +14,7 @@ class Tabuleiro:
     _send_address: tuple
     _total_squares: int
     _point_per_square : int
+    _moves : int
     _con: list[list[str]]
 
     def __init__(self, x: int, y: int):
@@ -23,6 +25,7 @@ class Tabuleiro:
         self._point_per_square = 10
         self._x = x
         self._y = y
+        self._moves = 0
         self._con = [
             ['x' for i in range(x*y)] 
                 for j in range(x*y)]
@@ -77,21 +80,34 @@ class Tabuleiro:
         # validar jogada
         if pta[0] is not ptb[0] and pta[1] is not ptb[1]:
             send_msg("Jogada Inválida, tente novamente\n", self._send_address)
+            return "invalid_play"
+        elif pta[0] >= self._x or ptb[0] >= self._x:
+            send_msg("Jogada Inválida, fora dos limites do tabuleiro, tente novamente\n", self._send_address)
+            return "invalid_play"
+        elif pta[1] >= self._y or ptb[1] >= self._y:
+            send_msg("Jogada Inválida, fora dos limites do tabuleiro, tente novamente\n", self._send_address)
+            return "invalid_play"
         elif pta[0] - ptb[0] > 1 or pta[1] - ptb[1] > 1:
             send_msg("Jogada Inválida, tente novamente\n", self._send_address)
+            return "invalid_play"
         elif ptb[0] - pta[0] > 1 or ptb[1] - pta[1] > 1:
             send_msg("Jogada Inválida, tente novamente\n", self._send_address)
+            return "invalid_play"
         elif pta == ptb:
             send_msg("Jogada Inválida, marque dois pontos diferentes para fazer uma linha, tente novamente\n", self._send_address)
+            return "invalid_play"
         elif self._con[pta1][ptb1] == "O":
             send_msg("Jogada Inválida, esta jogada já foi feita em outro turno, tente novamente\n", self._send_address)
+            return "invalid_play"
         else:
             self.connect(int(pta[0]), int(pta[1]), int(ptb[0]), int(ptb[1]))
             coords = "".join([f"Coordenadas jogadas pelo P{self._player}: ", str(pta), str(ptb), "\n"])
             send_all(coords,self._both_players_address)
+            return "valid_play"
 
     def game(self):
         from server import send_msg
+        from server import send_all
 
         while True:
              self.active_address()
@@ -99,16 +115,29 @@ class Tabuleiro:
              self.mostra_tabuleiro()
 
              try:
-                pta, ptb = self.read_play()
+                 pta, ptb = self.read_play()
              except (ValueError, IndexError):
                  send_msg("Erro na jogada, tente novamente com o formato (Linha Coluna) *com espaço*\n",self._send_address)
                  continue
 
-             self.validate_play(pta,ptb)
+             if self.validate_play(pta,ptb) == "invalid_play":
+                 continue
+             else:
+                 pass
 
-             self.verifica_quadrados()
+             if self.verifica_quadrados() == "point":
+
+                 if self._moves == 0:
+                     break
+
+                 continue
+             else:
+                 pass
 
              self.mostra_tabuleiro()
+
+             if self._moves == 0:
+                 break
 
              send_msg("Esperando a jogada do outro jogador...\n", self._send_address)
 
@@ -116,12 +145,16 @@ class Tabuleiro:
                  else 2
 
         winner : tuple
+        winner_name : str
         if self._player1_points > self._player2_points:
             winner = self._player1_address
+            send_all("\nJogador 1 ganhou !!!!!!!!\n",self._both_players_address)
         elif self._player2_points > self._player1_points:
             winner = self._player2_address
+            send_all("\nJogador 2 ganhou !!!!!!!!\n", self._both_players_address)
         else:
             winner = ("draw","draw")
+            send_all("\nEmpatou :(\n", self._both_players_address)
         return winner
 
 
@@ -172,15 +205,20 @@ class Tabuleiro:
             diff = squares - self._total_squares
             if self._player == 1:
                 self._player1_points = self._player1_points + (diff * self._point_per_square)
-                send_all(f"Jogador 1 pontuou e agora está com {self._player1_points : 04d}\n",self._both_players_address)
+                send_all(f"Jogador 1 pontuou e agora está com{self._player1_points : 04d}\n",self._both_players_address)
             else:
                 self._player2_points = self._player2_points + (diff * self._point_per_square)
-                send_all(f"Jogador 2 pontuou e agora está com {self._player2_points : 04d}\n",self._both_players_address)
+                send_all(f"Jogador 2 pontuou e agora está com{self._player2_points : 04d}\n",self._both_players_address)
             self._total_squares = squares
+            return "point"
+        else:
+            return "pass"
 
 
     def mostra_tabuleiro(self):
         from server import send_msg
+
+        counter = 0
 
         linha = 0
 
@@ -200,6 +238,7 @@ class Tabuleiro:
                     send_msg((T_BBLUE + T_FBLACK + ".____" + T_RESET),self._send_address)
                 else:
                     send_msg((T_BBLUE + T_FBLACK + ".    " + T_RESET),self._send_address)
+                    counter += 1
 
             send_msg((T_BBLUE + T_FBLACK + "." + T_RESET),self._send_address)
             send_msg((T_BGREEN + "|" + T_RESET),self._send_address)
@@ -216,6 +255,7 @@ class Tabuleiro:
                     send_msg((T_BBLUE + T_FBLACK + "|" + T_RESET), self._send_address)
                 else:
                     send_msg((T_BBLUE + T_FBLACK + " " + T_RESET), self._send_address)
+                    counter += 1
 
                 if i < self._x - 2:
                     send_msg((T_BBLUE + T_FBLACK + "    " + T_RESET), self._send_address)
@@ -224,13 +264,22 @@ class Tabuleiro:
             linha += 1
 
         send_msg(("\n" + T_BGREEN + "|" + T_RESET), self._send_address)
-        send_msg((T_BBLUE + T_FBLACK + ''.join(['.    ' for i in range(self._x - 2)]) + T_RESET), self._send_address)
+        #send_msg((T_BBLUE + T_FBLACK + ''.join(['.    ' for i in range(self._x - 2)]) + T_RESET), self._send_address)
+        for i in range(self._x - 2):
+            pa = self.get_num(linha, i)
+            pb = self.get_num(linha, i + 1)
+            if self._con[pa][pb] == 'O':
+                send_msg((T_BBLUE + T_FBLACK + ''.join(['.____']) + T_RESET), self._send_address)
+            else:
+                send_msg((T_BBLUE + T_FBLACK + ''.join(['.    ']) + T_RESET), self._send_address)
+                counter += 1
         send_msg((T_BBLUE + T_FBLACK + "." + T_RESET ), self._send_address)
         send_msg((T_BGREEN + "|" + T_RESET + "\n"), self._send_address)
         if self._player == 1:
             send_msg((T_BGREEN + f"| P1      - {self._player1_points : 04d} |" + T_RESET + "\n"), self._send_address)
         else:
             send_msg((T_BGREEN + f"| P2      - {self._player2_points : 04d} |" + T_RESET + "\n"), self._send_address)
+        self._moves = counter
 
     def __str__(self):
         ss = ""
